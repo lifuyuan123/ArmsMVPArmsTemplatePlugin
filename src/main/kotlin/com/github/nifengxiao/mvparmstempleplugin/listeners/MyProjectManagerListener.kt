50b45b03f13acdc9a6ac59560ba6ce93ee0a782e
lifuyuan <446960231@qq.com>
2021-04-11 15:08:50 +0800

    第一次提交

---
+++ src/main/kotlin/com/github/nifengxiao/mvparmstempleplugin/listeners/MyProjectManagerListener.kt
@@ -0,0 +1,24 @@
+package com.github.nifengxiao.mvparmstemplateplugin.listeners
+
+import com.github.nifengxiao.mvparmstemplateplugin.services.MyProjectService
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManagerListener
+
+internal class MyProjectManagerListener : ProjectManagerListener {
+
+    override fun projectOpened(project: Project) {
+        projectInstance = project
+        project.getService(MyProjectService::class.java)
+    }
+
+    override fun projectClosing(project: Project) {
+        projectInstance = null
+        super.projectClosing(project)
+    }
+
+    companion object {
+        var projectInstance: Project? = null
+    }
+
+}
---
+++ src/main/kotlin/com/github/nifengxiao/mvparmstempleplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nifengxiao.mvparmstemplateplugin.services
+
+import com.github.nifengxiao.mvparmstemplateplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
